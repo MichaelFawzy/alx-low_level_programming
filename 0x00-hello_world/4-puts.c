5353d033f7beabeeda9c94b42ab4c0c0b8bf1143
MichaelFawzy <michael.fawzy.com@outlook.com>
2023-07-13 19:50:55 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - A Programming is like building a multilingual puzzle
  * Return: 0 (Success)
- * \
+ * /
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
